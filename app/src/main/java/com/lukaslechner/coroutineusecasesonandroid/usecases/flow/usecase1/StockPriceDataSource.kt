8d989c2e6e79de31b743a22b4c7983482c63729c
Lukas Lechner <office@lukaslechner.com>
2022-07-18 14:51:56 +0200

    State before explaining flow builders

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase1/StockPriceDataSource.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase1/StockPriceDataSource.kt
@@ -2,13 +2,19 @@ package com.lukaslechner.coroutineusecasesonandroid.usecases.flow.usecase1
 
 import com.lukaslechner.coroutineusecasesonandroid.usecases.flow.mock.FlowMockApi
 import com.lukaslechner.coroutineusecasesonandroid.usecases.flow.mock.Stock
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 
 interface StockPriceDataSource {
     val latestPrice: Flow<List<Stock>>
 }
 
-class NetworkStockPriceDataSource(mockApi: FlowMockApi): StockPriceDataSource {
+class NetworkStockPriceDataSource(mockApi: FlowMockApi) : StockPriceDataSource {
 
-    override val latestPrice: Flow<List<Stock>> = TODO()
+    override val latestPrice: Flow<List<Stock>> = {
+        while (true) {
+            mockApi.getCurrentStockPrices()
+            delay(5_000)
+        }
+    }
 }
