61ef5aaf269fc899267ec065237f35022a8688b6
Lukas Lechner <office@lukaslechner.com>
2022-07-19 13:09:32 +0200

    Display list of stocks on the screen

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase1/FlowUseCase1ViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase1/FlowUseCase1ViewModel.kt
@@ -1,24 +1,21 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.flow.usecase1
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import kotlinx.coroutines.launch
-import timber.log.Timber
 
 class FlowUseCase1ViewModel(
     private val stockPriceDataSource: StockPriceDataSource
 ) : BaseViewModel<UiState>() {
 
+    val currentStockPriceAsLiveData: MutableLiveData<UiState> = MutableLiveData()
+
     init {
         viewModelScope.launch {
-            stockPriceDataSource.latestPrice.collect {
-                Timber.d("Received item: ${it.first()}")
+            stockPriceDataSource.latestPrice.collect { stockList ->
+                currentStockPriceAsLiveData.value = UiState.Success(stockList)
             }
         }
     }
-
-    val currentStockPriceAsLiveData: LiveData<UiState> = MutableLiveData()
-
 }
