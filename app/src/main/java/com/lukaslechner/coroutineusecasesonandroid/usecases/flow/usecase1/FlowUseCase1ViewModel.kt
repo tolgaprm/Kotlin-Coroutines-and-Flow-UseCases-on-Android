e98371b36ac8291d230e53be47c827f9b4c9beeb
Lukas Lechner <office@lukaslechner.com>
2022-07-20 14:43:05 +0200

    ViewModel after lifecycle operations lecture

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase1/FlowUseCase1ViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase1/FlowUseCase1ViewModel.kt
@@ -3,19 +3,29 @@ package com.lukaslechner.coroutineusecasesonandroid.usecases.flow.usecase1
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
 
 class FlowUseCase1ViewModel(
-    private val stockPriceDataSource: StockPriceDataSource
+    stockPriceDataSource: StockPriceDataSource
 ) : BaseViewModel<UiState>() {
 
     val currentStockPriceAsLiveData: MutableLiveData<UiState> = MutableLiveData()
 
     init {
-        viewModelScope.launch {
-            stockPriceDataSource.latestPrice.collect { stockList ->
-                currentStockPriceAsLiveData.value = UiState.Success(stockList)
+        stockPriceDataSource
+            .latestPrice
+            .map { stockList ->
+                UiState.Success(stockList) as UiState
             }
-        }
+            .onStart {
+                emit(UiState.Loading)
+            }
+            .onEach { uiState ->
+                currentStockPriceAsLiveData.value = uiState
+            }
+            .launchIn(viewModelScope)
     }
 }
