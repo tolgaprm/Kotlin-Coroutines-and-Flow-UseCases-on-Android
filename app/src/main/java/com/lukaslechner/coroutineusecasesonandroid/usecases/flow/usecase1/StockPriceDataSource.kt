203a04bdabb5138edbf376a713574d6deecd50d6
Lukas Lechner <office@lukaslechner.com>
2022-07-21 11:54:24 +0200

    After asLiveData() Terminal operator

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase1/StockPriceDataSource.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase1/StockPriceDataSource.kt
@@ -5,6 +5,7 @@ import com.lukaslechner.coroutineusecasesonandroid.usecases.flow.mock.Stock
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import timber.log.Timber
 
 interface StockPriceDataSource {
     val latestPrice: Flow<List<Stock>>
@@ -15,7 +16,10 @@ class NetworkStockPriceDataSource(mockApi: FlowMockApi) : StockPriceDataSource {
     override val latestPrice: Flow<List<Stock>> = flow {
         while (true) {
             val currentStockList = mockApi.getCurrentStockPrices()
+
+            Timber.d("Emitting new stock list")
             emit(currentStockList)
+
             delay(5_000)
         }
     }
