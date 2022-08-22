d6c0c5ed5be7349b1879b59f4cce60caddf1bf8a
Lukas Lechner <office@lukaslechner.com>
2022-08-22 15:02:35 +0200

    Preparing playground files for concurrency

---
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/playground/flow/concurrency/1_buffer.kt
@@ -0,0 +1,36 @@
+package com.lukaslechner.coroutineusecasesonandroid.playground.flow.context
+
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.buffer
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import kotlin.system.measureTimeMillis
+
+suspend fun main() = coroutineScope {
+
+    val flow = flow {
+        repeat(5) {
+            println("Emitter:    Start Cooking Pancake $it")
+            delay(100)
+            println("Emitter:    Pancake $it ready!")
+            emit(it)
+        }
+    }
+        .buffer()
+        .map {
+            // do some mapping
+            it
+        }
+
+    val duration = measureTimeMillis {
+        flow.collect {
+            println("Collector:  Start eating pancake $it")
+            delay(300)
+            println("Collector:  Finished eating pancake $it")
+        }
+    }
+
+    println("==============================================")
+    println("Breakfast took $duration ms")
+}
