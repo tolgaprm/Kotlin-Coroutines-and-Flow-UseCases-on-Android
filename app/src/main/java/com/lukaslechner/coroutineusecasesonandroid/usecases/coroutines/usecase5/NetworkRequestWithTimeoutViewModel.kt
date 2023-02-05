e90f8e27dc0bc4554fa693b2558a0a50ebb11819
Tolga Pirim <tolgapirim25@gmail.com>
2023-02-05 12:29:05 +0300

    Implemented retries and withTimeout

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase5/NetworkRequestWithTimeoutViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase5/NetworkRequestWithTimeoutViewModel.kt
@@ -1,14 +1,56 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase5
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withTimeout
+import kotlinx.coroutines.withTimeoutOrNull
+import timber.log.Timber
 
 class NetworkRequestWithTimeoutViewModel(
     private val api: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun performNetworkRequest(timeout: Long) {
+        uiState.value = UiState.Loading
+        //usingWithTimeOut(timeout)
+        usingWithTimeOutOrNull(timeout)
+    }
 
+    private fun usingWithTimeOut(timeout: Long) {
+        viewModelScope.launch {
+            try {
+                val recentAndroidVersions = withTimeout(timeout) {
+                    api.getRecentAndroidVersions()
+                }
+                uiState.value = UiState.Success(recentAndroidVersions)
+            } catch (e: TimeoutCancellationException) {
+                uiState.value = UiState.Error("Network Request timed out!")
+            } catch (e: Exception) {
+                Timber.e(e)
+                uiState.value = UiState.Error("Network Request failed!")
+            }
+        }
+    }
+
+    private fun usingWithTimeOutOrNull(timeout: Long) {
+        viewModelScope.launch {
+            try {
+                val recentAndroidVersions = withTimeoutOrNull(timeout) {
+                    api.getRecentAndroidVersions()
+                }
+                if (recentAndroidVersions != null){
+                    uiState.value = UiState.Success(recentAndroidVersions)
+                }else{
+                    uiState.value = UiState.Error("Network Request timed out!")
+                }
+            } catch (e: Exception) {
+                Timber.e(e)
+                uiState.value = UiState.Error("Network Request failed!")
+            }
+        }
     }
 
 }
--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
@@ -1,14 +1,42 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase6
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withTimeout
+import timber.log.Timber
 
 class RetryNetworkRequestViewModel(
     private val api: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun performNetworkRequest() {
+        uiState.value = UiState.Loading
+        viewModelScope.launch {
 
+            val numberOfRetries = 2
+            try {
+                repeat(numberOfRetries) {
+                    try {
+                        loadRecentAndroidVersions()
+                        return@launch
+                    } catch (e: Exception) {
+                        Timber.e(e)
+                    }
+                }
+                loadRecentAndroidVersions()
+            } catch (e: Exception) {
+                Timber.e(e)
+                uiState.value = UiState.Error("Network Request failed!")
+            }
+        }
+    }
+
+    private suspend fun loadRecentAndroidVersions() {
+        val recentAndroidVersions = api.getRecentAndroidVersions()
+        uiState.value = UiState.Success(recentAndroidVersions)
     }
 
 }
