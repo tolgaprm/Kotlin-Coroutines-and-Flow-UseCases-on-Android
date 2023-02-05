4e2d160ca0b34abe94e0fa7a8b9df82b9214f925
Tolga Pirim <tolgapirim25@gmail.com>
2023-02-05 12:29:51 +0300

    Extract retry logic into higher order function

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
@@ -18,20 +18,25 @@ class RetryNetworkRequestViewModel(
 
             val numberOfRetries = 2
             try {
-                repeat(numberOfRetries) {
-                    try {
-                        loadRecentAndroidVersions()
-                        return@launch
-                    } catch (e: Exception) {
-                        Timber.e(e)
-                    }
+                retry(numberOfRetries) {
+                    loadRecentAndroidVersions()
                 }
-                loadRecentAndroidVersions()
             } catch (e: Exception) {
                 Timber.e(e)
                 uiState.value = UiState.Error("Network Request failed!")
             }
         }
+    }
+
+    private suspend fun <T> retry(numberOfRetries: Int, block: suspend () -> T): T {
+        repeat(numberOfRetries) {
+            try {
+                return block()
+            } catch (e: Exception) {
+                Timber.e(e)
+            }
+        }
+        return block()
     }
 
     private suspend fun loadRecentAndroidVersions() {
